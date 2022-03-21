2593b1b24f3b08a748be6698d10e94eb8032a4fa
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2022-03-22 02:09:53 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -18,55 +18,81 @@ import {
 import MenuIcon from '@mui/icons-material/Menu';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 export default function App() {
   return (
     <div style={{ padding: '2px', margin: '1px' }}>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Guvi
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <br />
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            XYZ Organization,
-          </Typography>
-          <Typography variant="h5" component="div"></Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            India
-          </Typography>
-          <Typography variant="body2">
-            Learning is the key to success
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">CONTACT US</Button>
-        </CardActions>
-      </Card>
-      <br />
-      <Grid container spacing={2}>
-        <Grid item>
-          <Button variant="contained">Admin Login</Button>
+      <BrowserRouter>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Guvi
+              </Typography>
+              <Button color="inherit">
+                <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+                  Login
+                </Link>
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <br />
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/AdminLogin" element={<AdminLogin />} />
+              <Route path="/StudentLogin" element={<StudentLogin />} />
+              <Route path="/ContactUs" element={<ContactUs />} />
+            </Routes>
+          </CardContent>
+          <CardActions>
+            <Button size="small">
+              <Link
+                to="/ContactUs"
+                style={{ textDecoration: 'none', color: 'blue' }}
+              >
+                CONTACT US
+              </Link>
+            </Button>
+          </CardActions>
+        </Card>
+        <br />
+        <Grid container spacing={2}>
+          <Grid item>
+            <Button variant="contained">
+              <Link
+                to="/AdminLogin"
+                style={{ textDecoration: 'none', color: 'white' }}
+              >
+                Admin Login
+              </Link>
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained">
+              <Link
+                to="/StudentLogin"
+                style={{ textDecoration: 'none', color: 'white' }}
+              >
+                Student Login
+              </Link>
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Button variant="contained">Student Login</Button>
-        </Grid>
-      </Grid>
+      </BrowserRouter>
       <br />
       <Accordion>
         <AccordionSummary
@@ -109,4 +135,20 @@ export default function App() {
       </Accordion>
     </div>
   );
+}
+
+export function Login() {
+  return <div>"Login page"</div>;
+}
+
+export function AdminLogin() {
+  return <div>"Admin Login page"</div>;
+}
+
+export function StudentLogin() {
+  return <div>" Student Login page"</div>;
+}
+
+export function ContactUs() {
+  return <div>"Contact us page"</div>;
 }
